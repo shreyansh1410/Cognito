cda139052426b0089fc16fe686044282902883fa
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-01-18 13:42:42 +0530

    updated cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://cognito-theta.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
