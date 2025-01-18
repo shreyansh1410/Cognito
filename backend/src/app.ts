622e7e55e7f8a207037c4d1aca8523729ad4048f
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-01-18 16:16:20 +0530

    cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,20 +39,6 @@ app.use(
 
 app.options("*", cors());
 
-// Important: These headers must come after CORS middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Origin", "https://cognito-theta.vercel.app");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://cognito-86vu.onrender.com"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
