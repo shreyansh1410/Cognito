b2528c0150b32e62a97e5406c8be6db7e58bb504
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-02-17 23:54:23 +0530

    removed unused variables

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -13,7 +13,6 @@ import { Label } from "../components/ui/label";
 import { Textarea } from "../components/ui/textarea";
 import { useToast } from "../hooks/use-toast";
 import { Loader2 } from "lucide-react";
-import { Header } from "../components/Header";
 
 interface UserProfile {
   firstName: string;
